cafc2f9bee0537b8383dee2bef7bce3259da5684
Darya Matveeva <darya.d.chernikova@gmail.com>
2023-09-19 19:07:11 +0300

    fix task 6 corner cases

--- Task_06.fs
+++ Task_06.fs
@@ -2,9 +2,11 @@ open System
 
 let rec pow = function
  | (s,1) -> string s
+ | (s,n) when n <= 0 -> ""
  | (s,n) -> s + pow(s,n-1)
 
 let rec isIthChar = function
+ | (s,n,c) when n < 0 -> false
  | (s,n,c) when n >= String.length s -> false
  | (s,n,c) -> s.[n] = c
 
@@ -17,5 +19,6 @@ let rec occFromIth_rec = function
  | (s,n,c) -> bti(isIthChar(s,n,c)) + occFromIth_rec(s,n-1,c)
 
 let rec occFromIth = function
+ | (s,n,c) when n < 0 -> 0
  | (s,n,c) when n >= String.length s -> 0
  | (s,n,c) -> occFromIth_rec(s,(String.length s)-1,c) - occFromIth_rec(s,n-1,c)
