2d801c813273934ed5754aaa96198f79a104086e
Darya Matveeva <darya.d.chernikova@gmail.com>
2023-09-19 18:44:35 +0300

    fix task 6 rec

--- Task_06.fs
+++ Task_06.fs
@@ -4,19 +4,18 @@ let rec pow = function
  | (s,1) -> string s
  | (s,n) -> s + pow(s,n-1)
 
-let rec isIthChar(s: string,n,c) =
- s.[n] = c
+let rec isIthChar = function
+ | (s,n,c) when n >= String.length s -> false
+ | (s,n,c) -> s.[n] = c
 
 let bti = function
  | true -> 1
  | false -> 0
 
-let rec occFromIthRec = function
+let rec occFromIth_rec = function
  | (s,0,c) -> bti(isIthChar(s,0,c))
- | (s,n,c) -> bti(isIthChar(s,n,c)) + occFromIthRec(s,n-1,c)
+ | (s,n,c) -> bti(isIthChar(s,n,c)) + occFromIth_rec(s,n-1,c)
 
-let rec occFromIth(s,n,c) = 
- let last = String.length s - 1
- let sn = s[n..last]
- let len_s = String.length sn - 1
- occFromIthRec(s[n..last],len_s,c)
+let rec occFromIth = function
+ | (s,n,c) when n >= String.length s -> 0
+ | (s,n,c) -> occFromIth_rec(s,(String.length s)-1,c) - occFromIth_rec(s,n-1,c)
