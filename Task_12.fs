819b7f268a72e618867ca89af22958a34537e5bd
Darya Matveeva <darya.d.chernikova@gmail.com>
2023-10-10 09:56:16 +0300

    task_12 some fixes

--- Task_12.fs
+++ Task_12.fs
@@ -1,5 +1,6 @@
 let rec upto n = 
  let rec upto_rec = function
+  | m when n < m -> []
   | m when m = n -> [n] 
   | m -> m :: upto_rec(m + 1)
  upto_rec 1
@@ -7,10 +8,11 @@ let rec upto n =
 let rec dnto = function
  | n when n < 1 -> []
  | 1 -> 1 :: []
- | n -> n :: upto(n - 1)
+ | n -> n :: dnto(n - 1)
 
 let rec evenn n = 
  let rec evenn_rec = function
+  | m when m >= n -> []
   | m when m = (n - 1) * 2 -> [m]
   | m -> m :: evenn_rec(m + 2)
  evenn_rec 0
