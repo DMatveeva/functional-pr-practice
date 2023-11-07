755fac45521b135aad66989ff5ee45fe679909bb
Darya Matveeva <darya.d.chernikova@gmail.com>
2023-11-07 18:03:56 +0300

    task 20

---
+++ Task_20.fs
@@ -0,0 +1,14 @@
+let even_seq = Seq.initInfinite (fun i -> 2 * i)
+
+let rec fact = function
+ | 0 -> 1
+ | n -> n * fact(n - 1)
+
+let fac_seq = Seq.initInfinite (fun i ->  fact i)
+
+let c = function 
+ | 0 -> 0
+ | n when n % 2 = 1 -> -(n / 2) - 1
+ | n -> n / 2 
+
+let seq_seq = Seq.initInfinite (fun i -> c i)
